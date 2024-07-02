93ddd2e3a7303a02236f6c1d8473d8884ad23627
andrewlitv <61317270+andrewlitv@users.noreply.github.com>
2024-07-02 12:55:17 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
-export OPENAI_API_KEY="sk-proj-czRbDPmxmuEMNyGdvwWAT3BlbkFJSaoLH8nrjfoTUumpqFCo"
+export OPENAI_API_KEY="sk-proj-M9VyZPE7dJyoXYvFr7W2T3BlbkFJScqyfFHKzh01qGfVeDvC"
 python -m uvicorn main:app --host 0.0.0.0
